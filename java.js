cc8155032d637428d174485b7b55290c069de25c
Mitra Patel <mitrapatel@Mitras-MacBook-Pro.local>
2023-02-13 19:00:28 -0500

    All requirements added

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-// Function that adds boarder or removes boarder given the circle's border contitions 
+// Function that adds border or removes border given the circle's border contitions 
 
 let clicks = 0;
 
@@ -21,3 +21,44 @@ function borderFunction(pointNum) {
 	let lastPointDiv = document.getElementById("point-clicked");
 	lastPointDiv.innerHTML = lastPoint;
 }
+
+// Function that adds a point that is inputted by the user 
+function addCustomPoint() {
+
+	const radius = 10;
+
+	// Stores the coordinate values the user inputted
+	let chosenXCoordinate = document.getElementById('chooseX-Coordinate');
+	let chosenYCoordinate = document.getElementById('chooseY-Coordinate');
+
+	// Transforms user input into number type 
+	let intXCoordinate = Number(chosenXCoordinate.options[chosenXCoordinate.selectedIndex].text);
+	let intYCoordinate = Number(chosenYCoordinate.options[chosenYCoordinate.selectedIndex].text);
+
+	// Assigns the ID for the new user inputted point
+	let customPointId = '(' + intXCoordinate + ', ' + intYCoordinate + ')';
+
+	// scale the point so that it is pointed on the plot appropriately
+	factoredXCoord = intXCoordinate * 50;
+	factoredYCoord = 500 - (intYCoordinate * 50);
+
+	// Gets the area the chart is located in
+	let chart = document.getElementById('frame')
+
+	// Create a new circle 
+	var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+	// set the attributes of the new element
+	circle.setAttribute("id", customPointId);
+	circle.setAttribute("cx", factoredXCoord);
+	circle.setAttribute("cy", factoredYCoord); 
+	circle.setAttribute("r", radius); 
+	circle.setAttribute("class", "point"); 
+	circle.setAttribute("onclick", "borderFunction('" + customPointId + "')");
+
+	// display the new point on the plot
+	chart.appendChild(circle);
+}
+
+document.getElementById('addCustomPoint')
+			.addEventListener('click', addCustomPoint);
